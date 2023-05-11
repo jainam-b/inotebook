f2b8839644c1f7b765203236dadbf4533d5efe1b
Jainam Bagrecha <105155942+jainam-b@users.noreply.github.com>
2023-05-11 18:37:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const port = 4000;
 
 app.use(cors())
 app.use(express.json());
+app.use(express.static(path.join(__dirname,"./build")))
+app.get("*",function(res,req){
+    res.send(path.join(__dirname,"./build/index.html"))
+});
 
 
 // Available Routes
